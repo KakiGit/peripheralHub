4cdf0b425695db5553f273fa210b0c8739e85022
Kaki <kaki_github@icloud.com>
2020-09-27 16:39:34 +0300

    draft server logic

--- server/server.go
+++ server/server.go
@@ -3,7 +3,7 @@ package server
 import (
 	"fmt"
 	"net"
-	"strings"
+	"sort"
 	"sync"
 	"time"
 
@@ -11,17 +11,15 @@ import (
 )
 
 type Server struct {
-	secret        []byte
-	clients       []net.Addr
-	currentClient net.Addr
-	lock          *sync.Mutex
+	secret          []byte
+	clients         map[string]string
+	currentClient   net.Addr
+	lock            *sync.Mutex
+	serverAddresses []string
+	platform        common.Platform
 }
 
-func serve(address string, secret string) {
-	server := Server{
-		secret: common.ReadSecret(common.CreateEncodedSecret(common.Secret(secret))),
-		lock:   &sync.Mutex{},
-	}
+func (server *Server) serve(address string) {
 	doneChan := make(chan error, 1)
 	to := 1 * time.Second
 	timeout := &to
@@ -52,21 +50,29 @@ func serve(address string, secret string) {
 		msg := common.Decrypt(buffer[:n], server.secret)
 		fmt.Printf("packet-received: bytes=%d from=%s text=%v\n",
 			n, addr.String(), msg)
-		addrPart := strings.Split(addr.String(), ":")[0]
-		if common.Address(addrPart) == common.BytesToAddress(msg.SenderAddress) {
+		receiverAddr := string(common.BytesToAddress(msg.ReceiverAddress))
+		i := sort.SearchStrings(server.serverAddresses, receiverAddr)
+		if i < len(server.serverAddresses) && server.serverAddresses[i] == receiverAddr {
 			server.lock.Lock()
-			server.clients = append(server.clients, addr)
+			if !server.hasClient(addr) {
+				server.clients[addr.String()] = receiverAddr
+			}
+			// TODO: Change selection of currentClient to a better way
 			server.currentClient = addr
 			server.lock.Unlock()
 		}
-	}(&server)
-	for {
 
+	}(server)
+	for {
+		receiverAddr := common.Address(server.currentClient.String())
+		senderAddr := common.AddressToBytes(common.Address(server.findSenderAddr(server.currentClient)))
 		internalMsg := <-output.Com
 		msgToBeSent := common.Message{
-			Event:       internalMsg.Event,
-			EventEntity: internalMsg.EventEntity,
-			ExtraInfo:   internalMsg.ExtraInfo,
+			ReceiverAddress: common.AddressToBytes(receiverAddr),
+			SenderAddress:   senderAddr,
+			Event:           internalMsg.Event,
+			EventEntity:     internalMsg.EventEntity,
+			ExtraInfo:       internalMsg.ExtraInfo,
 		}
 		fmt.Printf("%v\n", msgToBeSent)
 		pc.WriteTo(common.Encrypt(msgToBeSent, server.secret), server.currentClient)
@@ -74,5 +80,51 @@ func serve(address string, secret string) {
 }
 
 func ListenAndServe(address string, secret string) {
-	serve(address, secret)
+	sec := common.Secret(secret)
+	encodedPwd := common.CreateEncodedSecret(sec)
+	fmt.Println("Secret:", encodedPwd)
+	server := Server{
+		secret:          common.ReadSecret(encodedPwd),
+		lock:            &sync.Mutex{},
+		serverAddresses: getServerAddresses(),
+	}
+	server.serve(address)
+}
+
+func getServerAddresses() []string {
+	var svAddrs []string
+	ifaces, _ := net.Interfaces()
+	for _, i := range ifaces {
+		addrs, _ := i.Addrs()
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			svAddrs = append(svAddrs, ip.String())
+			fmt.Println(ip)
+		}
+	}
+	return svAddrs
+}
+
+func (server *Server) hasClient(addr net.Addr) bool {
+	for clientA := range server.clients {
+		if clientA == addr.String() {
+			return true
+		}
+	}
+	return false
+}
+
+func (server *Server) findSenderAddr(addr net.Addr) string {
+	senderAddr, ok := server.clients[addr.String()]
+	if ok {
+		return senderAddr
+	} else {
+		return ""
+	}
 }
